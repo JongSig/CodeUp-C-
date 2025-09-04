77dfe54de5181cddbd5275314d332be338df7c42
Bell_End <bnghty46@naver.com>
2025-09-04 21:37:30 +0900

    [commit] 별 찍기 - 1

---
+++ 반복문/2438.cpp
@@ -0,0 +1,32 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int N;
+//     cin >> N;
+
+//     for(int i=0; i<N; i++)
+//     {
+//         for(int j=0; j<=i; j++) cout << "*";
+//         cout << "\n";
+//     }
+
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    for(int i=1; i<=N; i++){
+        cout << string(i, '*') << "\n";
+    }
+
+    return 0;
+}
